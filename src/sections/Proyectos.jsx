d310bcdd480979038913c42a9e63f3bf97a8a50e
AlejandraVazRuiz <roshlandia@gmail.com>
2024-02-14 12:27:01 -0600

    Diseños chidoris
    Se mejoraron la presentacion de los proyectos

--- src/sections/Proyectos.jsx
+++ src/sections/Proyectos.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import pokebola from '../assets/img/proyectos/pokebola.png'
 import logoPokemon from '../assets/img/proyectos/logoPokemon.png'
 import charmander from '../assets/img/proyectos/charmander.png'
+import fondoRick from '../assets/img/proyectos/fondoRick.png'
+import personajeRick from '../assets/img/proyectos/ricks.png'
+import logoRick from '../assets/img/proyectos/logoRick.png'
 
 
 
@@ -9,17 +12,30 @@ import charmander from '../assets/img/proyectos/charmander.png'
 
 const Proyectos = () => {
 
+ const ArrayProyects=[
+    {id:1,imagenFondo:pokebola,logo:logoPokemon,personaje:charmander},
+    {id:2,imagenFondo:fondoRick,logo:logoRick,personaje:personajeRick}
+  ]
 
 
 
   return (
-    <div className='center contenCara '>
+    <div className='center contenCara col-12 '>
         <h2 className='apt FirstText'>Proyectos</h2>
-        <div className='contenProyecto'>
-            <img className='fondoProyect' src={pokebola} alt="" />
-            <img className='logoPoryect' src={logoPokemon} alt="" />
-            <img className='personajeProyect' src={charmander} alt="" />  
-        </div>
+       {
+        ArrayProyects.map(P=>(
+         <div className='boxContenProyect'>
+           <div key={P.id} className='contenProyecto'>
+              <img className='fondoProyect' src={P.imagenFondo} alt="" />
+              <img className='logoPoryect' src={P.logo} alt="" />
+              <img className='personajeProyect' src={P.personaje} alt="" />  
+          </div>
+          <div>
+
+          </div>
+         </div>
+        ))
+       }
     </div>
   )
 }
